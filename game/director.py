1087aee0e9ebd2cb5bb77aee0a769ff964eb64ac
mrollins20 <crazydude874@gmail.com>
2022-10-13 09:47:55 -0700

    Created README.TXT and added to the director.py file. Also fixed an issue with the import on __main__.py.

--- game/director.py
+++ game/director.py
@@ -7,9 +7,10 @@ class Director:
     The director is responsible for controlling the sequence of play.
 
     Attributes:
-        art(object): A 
-        puzzle(string): A secret word randomly chosen from a list.
-        guess(string): a lettler in the puzzle guessed by the player
+        
+        guess(string): a lettler in the puzzle guessed by the player.
+        is_playing(boolean): A value of True or False, defining whether the player is still playing.
+        puzzle_word(string): A secret word randomly chosen from a list.
     """
     def __init__(self):
         """Constructs a new Director.
@@ -17,6 +18,9 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
+        self.guess = ""
+        self.is_playing = True
+        self.puzzle_word = ""
         
     def start_game(self):
         """Starts the game.
@@ -24,8 +28,52 @@ class Director:
         Args:
             self (Director): an instance of Director.
         """
+        if not self.is_playing:
+            return
+        while self.is_playing:
+            self.prompt_player()
+            self.update_values()
+            self.print_values()
+        self.continue_game()
+
     def prompt_player(self):
         """Prompts the player for inputs.
 
         Args:
+            self (Director): an instance of Director.
         """
+        if not self.is_playing:
+            return
+    
+    def update_values(self):
+        """Updates the puzzle, guess count, and visible letters.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+    
+    def print_values(self):
+        """Displays the puzzle graphics.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+
+    def continue_game(self):
+        """Asks the user if they would like to play again.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        if not self.is_playing:
+            return
+        restart_game = str(input("Would you like to play again?\n('yes' or 'no') "))
+        if restart_game == "yes":
+            self.start_game()
+        elif restart_game == "no":
+            self.is_playing = False
+            return
+        else:
+            print("Sorry. That input wasn't accepted.")
+            self.is_playing = False
+            return
