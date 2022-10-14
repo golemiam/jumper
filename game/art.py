dc54d3aca63aecf2ef9bf68e898d0ee9678e3751
golemiam <52841560+golemiam@users.noreply.github.com>
2022-10-14 08:30:09 -0600

    Update art.py
    Added some wires for the parachute. Still trying to figure out how to properly apply one method in another. Not sure it works at all, may need to combine methods.

--- game/art.py
+++ game/art.py
@@ -1,61 +1,9 @@
 import turtle
-
-class Full_image:
-    def __init__():
-        self.wires = Wires()
-        self.person = Person()
-        self.chute = Chute()
-
-    def draw_image():
-        Wires()
-        Person()
-        Chute()
-        def draw_circle(_t, _x, _y, radius, fillcolor, pencolor):
-            """
-            This is the atomic shape circle used for the ball on the sword and the shield in various places throughout.
-            _x, and _y are for coordinates
-            radius is used for the circle size
-            fillcolor is for the color inside of the circles
-            pencolor is for the circle borders
-            """
-            _t.color(pencolor, fillcolor)
-            _t.begin_fill()
-            _t.up()
-            _t.goto(_x,_y)
-            _t.down()
-            _t.circle(radius)
-            _t.end_fill()
-            _t.speed(0)
-
-class Wires:
-    def line():
-        """
-        """
-
-class Person:
-    def __init__():
-        self.circle = draw_circle()
-    def head():
-        """
-        """
-    def arm():
-        """
-        """
-    def leg():
-        """
-        """
-
-class Chute:
-    """
-    """
-    def half_circle():
-        """
-        """
-
-person()
+    
+_t = turtle.Turtle()
 
 class draw_shapes:
-    def draw_circle(_t, _x, _y, radius, fillcolor, pencolor):
+    def draw_circle(_t, _x, _y, radius, extent, fillcolor, pencolor):
         """
         This is the atomic shape circle used for the ball on the sword and the shield in various places throughout.
         _x, and _y are for coordinates
@@ -68,7 +16,7 @@ class draw_shapes:
         _t.up()
         _t.goto(_x,_y)
         _t.down()
-        _t.circle(radius)
+        _t.circle(radius, extent)
         _t.end_fill()
         _t.speed(0)
 
@@ -103,3 +51,62 @@ class draw_shapes:
         _t.goto(_x, _y)
         _t.down()
         _t.forward(length)
+        
+class Full_image:
+    def __init__(self):
+        self.wires = Wires()
+        self.person = Person()
+        self.chute = Chute()
+
+    def draw_image(self):
+        Wires()
+        Person()
+        Chute()
+
+class Chute:
+    """
+    draws the parachute top
+    """
+    def half_circle(self):
+        """
+        draws the half circles used to make the parachute.
+        """
+        draw_circle(_t, _x, _y, radius, extent, fillcolor, pencolor)
+class Wires:
+    def line(self, _t, x, y, length, tilt):
+        """
+        draws the wires to the parachute.
+        """
+        _t.up()
+        _t.goto(x,y)
+        _t.down()
+        _t.setheading(tilt)
+        _t.forward(length)
+
+class Person:
+    def __init__(self):
+        self.circle = draw_circle()
+    def head():
+        """
+        """
+    def arm(self):
+        """
+        """
+    def leg(self):
+        """
+        """
+
+
+
+
+
+
+        
+        
+Wires.line("self", _t, 0, 0, 110, 30)
+Wires.line("self", _t, 0, 0, 100, 55)
+Wires.line("self", _t, 0, 0, 90, 80)
+Wires.line("self", _t, 0, 0, 90, 105)
+Wires.line("self", _t, 0, 0, 100, 130)
+Wires.line("self", _t, 0, 0, 110, 155)
+
